a78d29e62117099e93518067e1d7f5a4014bc6e9
sidruns30 <siddhantsolanki321@gmail.com>
2021-09-12 12:28:27 -0700

    fixed untracked files

--- extra/makeplots.h
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-
-void plot_2d(double *xarr, double *yarr, char args[], const int npoints);
-void plot_2d_file(char *fname, char args[], const int npoints);
